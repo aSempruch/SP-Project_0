337c93011b7326324d0cecddf90e5e045ac753b0
Sarah Khasawneh <sk1683@cp.cs.rutgers.edu>
2017-09-26 11:13:20 -0400

    Merge Function update with memcpy

--- mergesort.c
+++ mergesort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "sorter.h"
 
-void mergesort(movie array[][col], int a, int b)
+void mergesort(movie array[][28], int a, int b)
 {
 //a(p) is left index, b(r) is right, m(q) is middle
 	int l = a, r =b, m;
@@ -18,42 +19,46 @@ void mergesort(movie array[][col], int a, int b)
 
 //This function loops into columns of the array to search for 
 //the index of the key word given to sort by
-int getKey(char *word, movie array[][col])
+int getKey(char *word, movie array[][28])
 {
-	int c = atoi(word), i;
-	for(i=0; i<col;i++)
+	int c = atoi(word), i, w;
+	for(i=0; i<28;i++)
 	{
-		if(atoi(array[i]) == c)
+		w = atoi(array[i]);
+		if( w == c)
 		{
 			return i;
 		}
 	}	
-return NULL;
+return 0;
 }
-void merge(movie array[][col], int p, int q, int r, char *word)
+void merge(movie array[][28], int p, int q, int r, char *word)
 {
 	int i,j,k;
 	char *key = word;
 	int m1 = q-p+1, m2 = r-q;
 	
-	movie *left = malloc(sizeof(movie*m1));
-	movie *right= malloc(sizeof(movie*m2));
+	movie *left = (movie*)  malloc(sizeof(movie)*m1);
+	movie *right = (movie*) malloc(sizeof(movie)*m2);
 
 	for(i=0; i<m1; i++)
 	{
-		left[i] = array[p+i];
+		memcpy(&array[p+i], &left[i], sizeof((array[p+i])));
 	}
 	for(j=0; j<m2;j++)
 	{
-		right[j] = array[q+j+1];
+		memcpy(&array[q+j+1], &right[j], sizeof((array[q+j+1])));
 	}
 
 	i=0, j=0,k=p;
 
-	while(i<m1 && j<size2)
+	while(i<m1 && j<m2)
 	{
 		//Compare Code added here
 		
 
 	}
+
+free(left);
+free(right);
 }
