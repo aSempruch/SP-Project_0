3105309d256e9da6bfe365baab36efe1b86c37f4
Alan <alan.sempruch.rhs@gmail.com>
2017-10-02 18:47:23 -0400

    Updated header file

--- sorter.h
+++ sorter.h
@@ -44,3 +44,15 @@ void mergesort(movie **array, int a, int b, char *word);
 void print(movie **info, int numOfEntries);
 
 #endif
+/*****
+*
+*	Define structures and function prototypes for your sorter
+*
+*
+*
+******/
+
+//Suggestion: define a struct that mirrors a record (row) of the data set
+
+
+//Suggestion: prototype a mergesort function
